ac1c8554fa60fe719801f16188b52be6a1859002
Tahmeed Mostafiz <tmostafiz@gmail.com>
2020-09-03 17:54:57 -0400

    added user model

---
+++ server/models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {type: String, required: true}, // Name of user
+    email: {type: String, required: true, unique: true}, // Email of user
+    password: {type: String, required: true, min: 8}, // Password of user
+});
+
+
+module.exports = mongoose.model('User', userSchema);
