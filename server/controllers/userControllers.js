674f358c3a78f4c24774250e3fd486d75d518daa
Tahmeed Mostafiz <tmostafiz@gmail.com>
2020-09-03 18:48:38 -0400

    user auth

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,8 +1,25 @@
+const HttpError = require('../models/http-error');
 const User = require('../models/user');
 
 
 const signup = async (req, res, next) => {
-    res.json({message: "Successful connection to signup new user"});
+    res.json({message: "Successful connection to signup user"});
+
+
+    // let user = new User({
+    //     name: req.body.name,
+    //     email: req.body.email,
+    //     password: req.body.password
+    // })
+
+    // user.save()
+    // .then(() => {
+    //     res.status(201).json({user});
+    // })
+    // .catch(error => {
+    //     const err = new HttpError(error, 500);
+    //     return next(err);
+    // })
 };
 
 const login = async (req, res, next) => {
