1888b84009c49e03e39f6afc3bf2313a6ddd2053
Omar Baranek <onader27@gmail.com>
2020-09-06 15:41:56 -0400

    added the welcoming sentense in the navbar when authenticated

--- client/src/components/UI/NavBar/NavBar.js
+++ client/src/components/UI/NavBar/NavBar.js
@@ -1,37 +1,47 @@
-import React from 'react';
+import React from "react";
 import { NavLink } from "react-router-dom";
-import classes from './NavBar.module.css';
+import { connect } from "react-redux";
+
+import classes from "./NavBar.module.css";
 
 const NavLinkStyle = {
-    textDecoration: "none", 
-    color: "white", 
-    textAlign: "center", 
-    padding:"5px", 
-    margin: "2px 15px"
-}
-const NavBar = () => {
-    return (
-        <div className={classes.navbar}>
-            <div>Logo</div>
-          <div className={classes.links}>
-            <NavLink
-              to="/signup"
-              style={NavLinkStyle}>
-              <div>
-                Sign Up
-              </div>
-            </NavLink>
-            <NavLink
-              to="/login"
-              style={NavLinkStyle}
-            >
-              <div>
-                Log In
-              </div>
-            </NavLink>
-          </div>
-        </div>
-    );
+  textDecoration: "none",
+  color: "white",
+  textAlign: "center",
+  padding: "5px",
+  margin: "2px 15px",
 };
 
-export default NavBar;
+const NavBar = (props) => {
+  let nav = (
+    <React.Fragment>
+      <NavLink to="/signup" style={NavLinkStyle}>
+        <div>Sign Up</div>
+      </NavLink>
+      <NavLink to="/login" style={NavLinkStyle}>
+        <div>Log In</div>
+      </NavLink>
+    </React.Fragment>
+  );
+
+  if (props.isAuthenticated) {
+    nav = <div style={NavLinkStyle}>Hello, {props.username}</div>;
+  }
+
+  return (
+    <div className={classes.navbar}>
+      <div>Logo</div>
+      <div className={classes.links}>{nav}</div>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    isAuthenticated: state.auth.token !== null,
+    username: state.auth.username,
+  };
+};
+
+export default connect(mapStateToProps, null)(NavBar);
+
