b80c9fe209355f50be3e9a559adcdbb24cb679f7
Dhruv Mittal <d2mittal@edu.uwaterloo.ca>
2020-09-16 18:07:08 -0400

    fixed login and signup with image uploads

--- client/src/store/reducers/listings.js
+++ client/src/store/reducers/listings.js
@@ -17,14 +17,12 @@ const reducer = (state = initialState, action) => {
         return updateObject(state, {listingsByUserID: action.listings});
       case actionTypes.GET_LISTINGS_BY_USERID_FAIL:
         return updateObject(state, {error: action.error});
-        case actionTypes.GET_LISTINGS_BY_FILTERS_SUCCESS:
-      return updateObject(state, { listings: action.listings });
-    case actionTypes.GET_LISTINGS_BY_ADDRESS_START:
-      return updateObject(state, { error: null, listings: null });
-    case actionTypes.GET_LISTINGS_BY_ADDRESS_SUCESS:
-      return updateObject(state, { listings: action.listings });
-    case actionTypes.GET_LISTINGS_BY_ADDRESS_FAIL:
-      return updateObject(state, { listings: null, error: action.error });
+      case actionTypes.GET_LISTINGS_BY_ADDRESS_START:
+        return updateObject(state, { error: null, listings: null });
+      case actionTypes.GET_LISTINGS_BY_ADDRESS_SUCESS:
+        return updateObject(state, { listings: action.listings });
+      case actionTypes.GET_LISTINGS_BY_ADDRESS_FAIL:
+        return updateObject(state, { listings: null, error: action.error });
       default:
         return state;
     }
